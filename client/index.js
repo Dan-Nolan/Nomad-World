bf63dd4bda43092d7f7ac4b9167a4d72c840a91c
Dan <DanNolan99@gmail.com>
2021-11-22 00:17:36 -0500

    login includes profile address

--- client/index.js
+++ client/index.js
@@ -1,4 +1,9 @@
 import "./index.scss";
 import state from "./state/index";
+import jsCookie from 'js-cookie';
+
+const PROFILE_KEY = process.env.REACT_APP_USER_TOKEN || 'x-nomad-profile';
+
+console.log(jsCookie.get(PROFILE_KEY));
 
 state.initialize();
