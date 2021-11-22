59a619fb8d4a46827fcf2feb6a33be2c4d4d2ed1
Dan <DanNolan99@gmail.com>
2021-11-22 00:31:25 -0500

    profile test

--- client/index.js
+++ client/index.js
@@ -2,8 +2,6 @@ import "./index.scss";
 import state from "./state/index";
 import jsCookie from 'js-cookie';
 
-const PROFILE_KEY = process.env.REACT_APP_USER_TOKEN || 'x-nomad-profile';
-
-console.log(jsCookie.get(PROFILE_KEY));
+console.log('profile test', jsCookie.get('x-nomad-profile'));
 
 state.initialize();
