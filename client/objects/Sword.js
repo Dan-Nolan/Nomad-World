a5d37c2243a7194b7de35c19745f6ea1ddcee0bd
Dan <DanNolan99@gmail.com>
2021-11-23 02:31:30 -0500

    mint sword to world

--- client/objects/Sword.js
+++ client/objects/Sword.js
@@ -1,7 +1,8 @@
 import state from '/client/state/index';
 import {ethers} from 'ethers';
 
-const SWORD_ADDRESS = "0x49Cda4f63Fe81b653007D04c49591a2096Ea39B7";
+const SWORD_ADDRESS = "0xc95439940280a6964b270b0373F25258d6F53c6C";
+const NOMAD_ADDRESS = "0x4731478A76e4bC5f012a569D061bE19c03c9177F";
 
 class Sword {
     x = 0;
@@ -18,14 +19,13 @@ class Sword {
         
         await ethereum.request({ method: 'eth_requestAccounts' });
         
-        const abi = [
-            "function mint(address) external returns(uint)"
-        ];
-        
         const provider = new ethers.providers.Web3Provider(ethereum);
         const signer = await provider.getSigner(0);
-        const sword = new ethers.Contract(SWORD_ADDRESS, abi, signer);
-        sword.mint(await signer.getAddress());
+        
+        const nomad = new ethers.Contract(NOMAD_ADDRESS, [
+            "function mintResource(address resource, uint universeId, uint destinationWorldId) external payable"
+        ], signer);
+        await nomad.mintResource(SWORD_ADDRESS, 0, 0);
     }
     update() {
 
